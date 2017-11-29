451fa5a02bca417b350b9b38f642896ef533ff61
Q.Roy <ruanqisevik@gmail.com>
2017-11-29 12:26:24 +0800

    feat(RQListViewControllerSwift): modified template

--- RQListViewControllerSwift/___FILEBASENAME___.swift
+++ RQListViewControllerSwift/___FILEBASENAME___.swift
@@ -3,53 +3,101 @@
 import UIKit
 
 class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
+    //MARK: define variable
+    var list: UITableView
     
-    lazy var list: UITableView = UITableView.init(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height))
+    //MARK: define constants
+    let reuseIdentifier = "reuseIdentifier"
     
-    let reuseIdentifier = "cellReuseIdentifier"
+    //MARK: initialize
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        self.view.addSubview(list)
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        self.list = UITableView.init(frame: CGRect.init(origin: CGPoint.zero, size: UIScreen.main.bounds.size))
+        
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         
         self.list.delegate = self
         self.list.dataSource = self
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        self.list.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        
+        // Do any additional setup after initialize.
     }
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    //MARK: life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Do any additional setup before view appears.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Do any additional setup after view appears.
+    }
+    
+    /*
+     override func viewWillDisappear(_ animated: Bool) {
+     super.viewWillAppear(animated)
+     
+     // Do any additional setup before view disappears.
+     }
+     
+     override func viewDidDisappear(_ animated: Bool) {
+     super.viewDidAppear(animated)
+     
+     // Do any additional setup after view disappears.
+     }
+     */
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //MARK: event response
+    
+    
+    //MARK: private methods
+    
+    
+    //MARK: getters and setters
+    
+    
 }
 
 extension ___FILEBASENAMEASIDENTIFIER___: UITableViewDataSource {
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 0
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 0
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
- 
-        // Configure the cell...
- 
+        
         return cell
     }
 }
 
 extension ___FILEBASENAMEASIDENTIFIER___: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("section: \(indexPath.section), row: \(indexPath.row) clicked")
+    }
 }
+
